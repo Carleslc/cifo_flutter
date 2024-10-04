a119498b86ecffcee4ecbb6d62eebe023830b734
Carleslc <lazaro.costa.carles@gmail.com>
2024-10-04 15:18:46 +0200

    Update apps

--- weather_example/lib/location.dart
+++
@@ -1,6 +0,0 @@
-typedef LatLng = ({double lat, double long});
-
-const LatLng hospitalet = (lat: 41.35888888, long: 2.0991666);
-
-/// Ubicació que s'utilitza per obtenir la previsió meteorològica
-const LatLng location = hospitalet;
